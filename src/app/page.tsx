0825522571a802dc2deec2f8a65ed389cfb86db0
Alex <148038194+Einfachalf@users.noreply.github.com>
2023-10-18 08:12:53 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,47 +22,44 @@ export default async function Home() {
   }
   return (
     <div className="w-screen min-h-screen bg-gradient-to-r from-rose-100 to-teal-100">
-      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-        <div className="flex flex-col items-center text-center">
-          <div className="flex items-center">
-            <h1 className="mr-3 text-5xl font-semibold">Chat with any PDF</h1>
-            <UserButton afterSignOutUrl="/" />
-          </div>
+  <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+    <div className="flex flex-col items-center text-center">
+      <div className="flex items-center">
+        <h1 className="mr-3 text-5xl font-semibold">Chat mit jedem PDF</h1>
+        <UserButton afterSignOutUrl="/" />
+      </div>
 
-          <div className="flex mt-2">
-            {isAuth && firstChat && (
-              <>
-                <Link href={`/chat/${firstChat.id}`}>
-                  <Button>
-                    Go to Chats <ArrowRight className="ml-2" />
-                  </Button>
-                </Link>
-                <div className="ml-3">
-                  <SubscriptionButton isPro={isPro} />
-                </div>
-              </>
-            )}
-          </div>
+      <div className="flex mt-2">
+        {isAuth && firstChat && (
+          <>
+            <Link href={`/chat/${firstChat.id}`}>
+              <Button>
+                Gehe zu Chats <ArrowRight className="ml-2" />
+              </Button>
+            </Link>
+            <div className="ml-3">
+              <SubscriptionButton isPro={isPro} />
+            </div>
+          </>
+        )}
+      </div>
 
-          <p className="max-w-xl mt-1 text-lg text-slate-600">
-            Join millions of students, researchers and professinals to instantly
-            anwer questions and understand research with AI
-          </p>
+      <p className="max-w-xl mt-1 text-lg text-slate-600">
+        Tritt Millionen von Studenten, Forschern und Fachleuten bei, um sofort Fragen zu beantworten und Forschung mit KI zu verstehen.
+      </p>
 
-          <div className="w-full mt-4">
-            {isAuth ? (
-              <FileUpload />
-            ) : (
-              <Link href="/sign-in">
-                <Button>
-                  Login to get Started!
-                  <LogIn className="w-4 h-4 ml-2" />
-                </Button>
-              </Link>
-            )}
-          </div>
-        </div>
+      <div className="w-full mt-4">
+        {isAuth ? (
+          <FileUpload />
+        ) : (
+          <Link href="/sign-in">
+            <Button>
+              Einloggen, um zu starten!
+              <LogIn className="w-4 h-4 ml-2" />
+            </Button>
+          </Link>
+        )}
       </div>
     </div>
-  );
-}
+  </div>
+</div>
