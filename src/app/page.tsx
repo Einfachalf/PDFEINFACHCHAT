66cc26f87926a1747b9b760b3e1aadfbd54600e4
Alex <148038194+Einfachalf@users.noreply.github.com>
2023-10-18 09:04:01 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import { useEffect, useState } from "react";
 import { Button } from "@/components/ui/button";
 import { UserButton, auth } from "@clerk/nextjs";
 import Link from "next/link";
@@ -10,42 +9,32 @@ import { db } from "@/lib/db";
 import { chats } from "@/lib/db/schema";
 import { eq } from "drizzle-orm";
 
-export default function Home() {
-  const [isAuth, setIsAuth] = useState(false);
-  const [isPro, setIsPro] = useState(false);
-  const [firstChat, setFirstChat] = useState(null);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const { userId } = await auth();
-      setIsAuth(!!userId);
-      setIsPro(await checkSubscription());
-
-      if (userId) {
-        const chatsResult = await db.select().from(chats).where(eq(chats.userId, userId));
-        if (chatsResult) {
-          setFirstChat(chatsResult[0]);
-        }
-      }
-    };
-
-    fetchData();
-  }, []);
-
+export default async function Home() {
+  const { userId } = await auth();
+  const isAuth = !!userId;
+  const isPro = await checkSubscription();
+  let firstChat;
+  if (userId) {
+    firstChat = await db.select().from(chats).where(eq(chats.userId, userId));
+    if (firstChat) {
+      firstChat = firstChat[0];
+    }
+  }
   return (
     <div className="w-screen min-h-screen bg-gradient-to-r from-rose-100 to-teal-100">
       <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
         <div className="flex flex-col items-center text-center">
           <div className="flex items-center">
-            <h1 className="mr-3 text-5xl font-semibold">Chat mit jedem PDF</h1>
+            <h1 className="mr-3 text-5xl font-semibold">Chat with any PDF</h1>
             <UserButton afterSignOutUrl="/" />
           </div>
+
           <div className="flex mt-2">
             {isAuth && firstChat && (
               <>
                 <Link href={`/chat/${firstChat.id}`}>
                   <Button>
-                    Gehe zu Chats <ArrowRight className="ml-2" />
+                    Go to Chats <ArrowRight className="ml-2" />
                   </Button>
                 </Link>
                 <div className="ml-3">
@@ -54,16 +43,19 @@ export default function Home() {
               </>
             )}
           </div>
+
           <p className="max-w-xl mt-1 text-lg text-slate-600">
-            Tritt Millionen von Studenten, Forschern und Fachleuten bei, um sofort Fragen zu beantworten und Forschung mit KI zu verstehen.
+            Join millions of students, researchers and professinals to instantly
+            anwer questions and understand research with AI
           </p>
+
           <div className="w-full mt-4">
             {isAuth ? (
               <FileUpload />
             ) : (
               <Link href="/sign-in">
                 <Button>
-                  Einloggen, um zu starten!
+                  Login to get Started!
                   <LogIn className="w-4 h-4 ml-2" />
                 </Button>
               </Link>
@@ -72,4 +64,5 @@ export default function Home() {
         </div>
       </div>
     </div>
-);
+  );
+}
